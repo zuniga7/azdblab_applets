32dc1cbb915ebd94d5aa8468aeaf647cda062afb
zuniga7 <zuniga7@email.arizona.edu>
2014-07-03 10:59:19 -0700

    - plantreeview1

---
+++ Test/src/applets/PlanTreeViewApplet.java
@@ -0,0 +1,17 @@
+package applets;
+
+import java.io.IOException;
+
+public class PlanTreeViewApplet extends ObjectApplet {
+
+	@Override
+	public Object returnRequestObject() throws ClassNotFoundException,
+			IOException {
+		return model.postRequest(
+				"https://aw.cs.arizona.edu/AZDBLAB/response.jsp",
+				"dataTarget=applet&id=visualize&runID=" + getParameter("runID")
+						+ "&iQueryNum=" + getParameter("iQueryNum")
+						+ "&queryExecutionNum" + getParameter("queryExecutionNum"));
+
+	}
+}
